9c593d82844f2980c9f8ffb486ba0bcf295962d3
cs4alhaider <cs.alhaider@gmail.com>
2020-08-29 04:37:31 +0300

    Added helmet and yup and error handling

--- src/app.js
+++ src/app.js
@@ -1,6 +1,16 @@
 const fs = require("fs");
 const lodash = require("lodash");
-const app = require("express")();
+const express = require("express");
+const helmet = require("helmet");
+const yup = require("yup");
+var {
+  nanoid
+} = require("nanoid");
+const {
+  nextTick
+} = require("process");
+
+require('dotenv').config();
 
 const allServices = fs.readFileSync(__dirname + "/services-list.json", {
   encoding: "utf8",
@@ -9,9 +19,69 @@ const allServices = fs.readFileSync(__dirname + "/services-list.json", {
 
 const servicesModel = JSON.parse(allServices);
 
+const app = express();
+
+// Basic security
+app.use(helmet());
+
+// Only accept json data
+app.use(express.json());
+
+// public
+app.use(express.static("./public"));
+
 // Show all available services and test data
-app.get("/", (req, res) => {
+app.get("/all", (req, res) => {
   res.send(servicesModel);
+});
+
+app.get("/:id", (req, res) => {
+  res.send(id);
+});
+
+const schema = yup.object().shape({
+  id: yup.string().trim(),
+  servicePath: yup.string().trim().matches(/[\w\-]/i).required(),
+  method: yup.string().trim().matches(/[\w]/i).required(),
+});
+
+app.post("/", async (req, res, next) => {
+  let {
+    id,
+    servicePath,
+    method
+  } = req.body;
+  try {
+    await schema.validate({
+      id,
+      servicePath,
+      method,
+    });
+    if (!id) {
+      id = nanoid(12);
+    }
+    id = id.toLowerCase();
+    res.json({
+      id,
+      servicePath,
+      method,
+    });
+  } catch (error) {
+    next(error);
+  }
+});
+
+// Handling all errors by calling next(), I know I'm lazy 😬
+app.use((error, req, res, next) => {
+  if (error.status) {
+    res.status(error.status);
+  } else {
+    res.status(500);
+  }
+  res.json({
+    message: error.message,
+    stack: process.env.NODE_ENV === "PROD" ? "😣" : error.stack,
+  });
 });
 
 // ================================================================ //
