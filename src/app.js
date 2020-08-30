9ebe79ac21b49b745d95a6ea5ced7730439cc285
cs4alhaider <cs.alhaider@gmail.com>
2020-08-31 01:07:59 +0300

    Cleaning app.js

--- src/app.js
+++ src/app.js
@@ -1,23 +1,7 @@
-const fs = require("fs");
-const lodash = require("lodash");
 const express = require("express");
 const helmet = require("helmet");
-const yup = require("yup");
-var {
-  nanoid
-} = require("nanoid");
-const {
-  nextTick
-} = require("process");
 
 require("dotenv").config();
-
-const allServices = fs.readFileSync(__dirname + "/services-list.json", {
-  encoding: "utf8",
-  flag: "r",
-});
-
-const servicesModel = JSON.parse(allServices);
 
 const app = express();
 
@@ -31,84 +15,12 @@ app.use(express.json());
 app.use(express.static("./public"));
 
 // Show all available services and test data
-app.get("/all", (req, res) => {
-  res.send(servicesModel);
-});
+app.use(require("./route/services"));
 
-app.get("/all/testData", (req, res) => {
-  res.send(servicesModel.testData);
-});
-
-app.get("/all/services", (req, res) => {
-  res.send(servicesModel.services);
-});
-
-// TODO: refactor
-app.get("/all/services/:id", (req, res) => {
-  const {
-    id: service
-  } = req.params;
-  const object = lodash.find(
-    servicesModel.services,
-    (obj) => obj.id === parseInt(service)
-  );
-  if (!object) {
-    res.status(404).json({
-      error: `No service with ${id}`
-    });
-    return;
-  }
-  res.send(object);
-});
-
-const schema = yup.object().shape({
-  id: yup.string().trim(),
-  servicePath: yup
-    .string()
-    .trim()
-    .matches(/[\w\-]/i)
-    .required(),
-  method: yup.string().trim().matches(/[\w]/i).required(),
-});
-
-app.post("/", async (req, res, next) => {
-  let {
-    id,
-    servicePath,
-    method
-  } = req.body;
-  try {
-    await schema.validate({
-      id,
-      servicePath,
-      method,
-    });
-    if (!id) {
-      id = nanoid(12);
-    }
-    id = id.toLowerCase();
-    res.json({
-      id,
-      servicePath,
-      method,
-    });
-  } catch (error) {
-    next(error);
-  }
-});
+app.use(require("./route/main"));
 
 // Handling all errors by calling next(), I know I'm lazy 😬
-app.use((error, req, res, next) => {
-  if (error.status) {
-    res.status(error.status);
-  } else {
-    res.status(500);
-  }
-  res.json({
-    message: error.message,
-    stack: process.env.NODE_ENV === "PROD" ? "😣" : error.stack,
-  });
-});
+app.use(require("./route/error"));
 
 // ================================================================ //
 
