284b0ad5f3bff3f23e38598f673a1e92a4def64f
cs4alhaider <cs.alhaider@gmail.com>
2020-09-01 02:09:41 +0300

    created once method to handle getting data by id for many endpoints

--- src/route/services.js
+++ src/route/services.js
@@ -3,36 +3,20 @@ const lodash = require("lodash");
 
 const servicesModel = require("../services-data/servicesModel");
 
-app.get("/all", (req, res) => {
-  res.send(servicesModel);
-});
-
-app.get("/all/testData", (req, res) => {
-  res.send(servicesModel.testData);
-});
-
-app.get("/all/services", (req, res) => {
-  res.send(servicesModel.services);
-});
-
-app.get("/all/services/:id", (req, res) => {
+const getObject = (head, req, res) => {
   const {
-    id: service
+    id: id
   } = req.params;
-  const object = lodash.find(
-    servicesModel.services,
-    (obj) => obj.id === parseInt(service)
-  );
-  if (!object) {
+  const object = lodash.find(head, (obj) => obj.id === parseInt(id));
+  if (!object)
     res.status(404).json({
-      error: `No service with ${id}`,
+      error: `No data found with id: ${id}`,
     });
-    return;
-  }
-  res.send(object);
-});
+  else
+    res.send(object);
+}
 
-function objectUsing(request) {
+const getObjectForRequestedPath = (request) => {
   const requestedPath = request.path;
   const object = lodash.find(
     servicesModel.services,
@@ -41,19 +25,40 @@ function objectUsing(request) {
   return object;
 }
 
+// MARK: All Data
+app.get("/", (req, res) => {
+  res.send(servicesModel);
+});
+
+// MARK: Test Data
+app.get("/testData", (req, res) => {
+  res.send(servicesModel.testData);
+});
+
+app.get("/testData/staging/:id", (req, res) => {
+  getObject(servicesModel.testData.staging, req, res)
+});
+
+// MARK: Services
+app.get("/services", (req, res) => {
+  res.send(servicesModel.services);
+});
+
+app.get("/services/:id", (req, res) => {
+  getObject(servicesModel.services, req, res);
+});
+
 // Handle all other requests if there is no specific route
 app.use((req, res) => {
-  const obj = objectUsing(req);
-
+  const obj = getObjectForRequestedPath(req);
   // check for object if not found
   if (!obj) {
     res.status(404).json({
       requestedPath: `${req.path}`,
-      error: 'No available service',
+      error: 'No service available for requested path 😟',
     });
     return;
   }
-
   if (req.method == obj.method) {
     res.sendFile(obj.filePath, {
       root: "../src",
