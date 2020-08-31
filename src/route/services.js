3576fd66c09c0f5390c08e76d2920472320058ab
cs4alhaider <cs.alhaider@gmail.com>
2020-09-01 01:29:27 +0300

    Routing undefined services to existing one if found

--- src/route/services.js
+++ src/route/services.js
@@ -32,4 +32,37 @@ app.get("/all/services/:id", (req, res) => {
   res.send(object);
 });
 
+function objectUsing(request) {
+  const requestedPath = request.path;
+  const object = lodash.find(
+    servicesModel.services,
+    (obj) => obj.servicePath === requestedPath
+  );
+  return object;
+}
+
+// Handle all other requests if there is no specific route
+app.use((req, res) => {
+  const obj = objectUsing(req);
+
+  // check for object if not found
+  if (!obj) {
+    res.status(404).json({
+      requestedPath: `${req.path}`,
+      error: 'No available service',
+    });
+    return;
+  }
+
+  if (req.method == obj.method) {
+    res.sendFile(obj.filePath, {
+      root: "../src",
+    });
+  } else {
+    res.status(405).json({
+      error: "405 Method Not Allowed",
+    });
+  }
+});
+
 module.exports = app;
