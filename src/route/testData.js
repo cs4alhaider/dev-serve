3c32d8d41f8da9726e5ad9e0f35033a01071fc9f
cs4alhaider <cs.alhaider@gmail.com>
2020-09-11 02:26:24 +0300

    mapping all testdata related services

--- src/route/testData.js
+++ src/route/testData.js
@@ -1,1 +1,54 @@
-// TODO: move all things related to test data APIs into this file
+const router = require("express").Router();
+
+const servicesModel = require("../services-data/servicesModel");
+
+const getObject = (head, req, res) => {
+    const {
+        id: id
+    } = req.params;
+    const object = head.find(obj => obj.id === parseInt(id));
+    if (!object)
+        res.status(404).json({
+            error: `No data found with id: ${id}`,
+        });
+    else
+        res.send(object);
+}
+
+// MARK: Test Data
+router.get("/", (req, res) => {
+    res.send(servicesModel.testData);
+});
+
+// TODO: router.post to add new testData
+router.post("/", (req, res) => {
+    res.send("TODO");
+});
+
+// TODO: router.patch to add new testData
+router.patch("/", (req, res) => {
+    res.send("TODO");
+});
+
+// TODO: router.delete to add new testData
+router.delete("/", (req, res) => {
+    res.send("TODO");
+});
+
+router.get("/staging", (req, res) => {
+    res.send(servicesModel.testData.filter(obj => obj.env.includes("staging")))
+});
+
+router.get("/staging/:id", (req, res) => {
+    getObject(servicesModel.testData.filter(obj => obj.env.includes("staging")), req, res)
+});
+
+router.get("/production", (req, res) => {
+    res.send(servicesModel.testData.filter(obj => obj.env.includes("production")))
+});
+
+router.get("/production/:id", (req, res) => {
+    getObject(servicesModel.testData.filter(obj => obj.env.includes("production")), req, res)
+});
+
+module.exports = router;
